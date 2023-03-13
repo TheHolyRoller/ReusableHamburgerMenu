8c8d1c6032fc1d7d4df52db8378206e7f372ccae
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-03-13 13:09:14 +0100

    first commit

---
+++ ReusableHamburger/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Hamburger from './Components/Hamburger'
+import React from 'react'; 
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <Hamburger/>
+     
+    </div>
+  )
+}
+
+export default App
